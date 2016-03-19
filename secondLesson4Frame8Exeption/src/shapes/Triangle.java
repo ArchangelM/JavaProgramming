94ea6d94bf494e26a4eae4a693008f0cb24f0860
Mihail Osipov <osarch@hotbox.ru>
2016-03-19 22:08:43 +0200

    Abstract

--- secondLesson4Frame8Exeption/src/shapes/Triangle.java
+++ secondLesson4Frame8Exeption/src/shapes/Triangle.java
@@ -12,30 +12,32 @@ public class Triangle  extends AbstractShape {
 
     public Triangle() {
         color = new Color(200, 100, 200);
-        setShapeDepth(1);
+        setShapeDepth(2);
         stroke = new BasicStroke(getShapeDepth());
 
         xPos = new int[VERTEX_NUM];
         yPos = new int[VERTEX_NUM];
 
-        xPos[0] = 450; yPos[0] = 200;
-        xPos[1] = 500; yPos[1] = 20;
-        xPos[2] = 550; yPos[2] = 200;
+        xPos[0] = 440; yPos[0] = 210;
+        xPos[1] = 500; yPos[1] = 10;
+        xPos[2] = 560; yPos[2] = 210;
     }
 
     @Override
     public void draw(Graphics g) {
         System.out.println("I`m draw triangle");
 
-        g.setColor(getColor());
-        g.drawPolygon(xPos, yPos, VERTEX_NUM);
+        Graphics2D g2d = (Graphics2D)g;
+        g2d.setStroke(getStroke());
+        g2d.setColor(getColor());
+        g2d.drawPolygon(xPos, yPos, VERTEX_NUM);
 
         xPos[0] = 460; yPos[0] = 190;
-        xPos[1] = 500; yPos[1] = 30;
+        xPos[1] = 500; yPos[1] = 40;
         xPos[2] = 540; yPos[2] = 190;
 
-        g.setColor(new Color(250, 50, 250));
-        g.fillPolygon(xPos, yPos, VERTEX_NUM);
+        g2d.setColor(new Color(250, 50, 250));
+        g2d.fillPolygon(xPos, yPos, VERTEX_NUM);
     }
 
 }
