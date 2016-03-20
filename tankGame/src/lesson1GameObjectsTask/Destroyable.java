bc5ab288caab6442595aeeabda51669e8e3e593b
Mihail Osipov <osarch@hotbox.ru>
2016-03-20 02:08:23 +0200

    Interfaces: Drawable Destriyable
    and implementation

--- tankGame/src/lesson1GameObjectsTask/Destroyable.java
+++ tankGame/src/lesson1GameObjectsTask/Destroyable.java
@@ -4,6 +4,6 @@ package lesson1GameObjectsTask;
  * Created by Rodichka on 20.03.2016.
  */
 public interface Destroyable {
-    public boolean destroy()  throws Exception;
-    public boolean stricken()  throws Exception;
+    boolean destroy()  throws Exception;
+    boolean stricken()  throws Exception;
 }
--- tankGame/src/lesson1GameObjectsTask/Drawable.java
+++ tankGame/src/lesson1GameObjectsTask/Drawable.java
@@ -6,5 +6,5 @@ import java.awt.*;
  * Created by Rodichka on 20.03.2016.
  */
 public interface Drawable {
-    public void draw(Graphics g);
+    void draw(Graphics g);
 }
