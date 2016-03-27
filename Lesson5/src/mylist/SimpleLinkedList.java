3df3012a68e4a548ef29318b5cd27634dab7cbf2
Mihail Osipov <osarch@hotbox.ru>
2016-03-28 02:29:29 +0300

    MyList Inner Class

--- Lesson5/src/mylist/SimpleLinkedList.java
+++ Lesson5/src/mylist/SimpleLinkedList.java
@@ -40,27 +40,9 @@ public class SimpleLinkedList {
     }
 
 
-    public class Node {
-        private Object o;
-        private Node node;
+    private class Node {
+        Object o;
+        Node node;
 
-        public Node() {
-        }
-
-        public Object getO() {
-            return o;
-        }
-
-        public void setO(Object o) {
-            this.o = o;
-        }
-
-        public Node getNode() {
-            return node;
-        }
-
-        public void setNode(Node node) {
-            this.node = node;
-        }
     }
 }
