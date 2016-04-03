818cf7544f30b0485cf737139fe1ae80f4002206
Mihail Osipov <osarch@hotbox.ru>
2016-04-04 01:20:16 +0300

    hashcode function

---
+++ Lesson5/src/hashcode/LauncherHash.java
@@ -0,0 +1,19 @@
+package hashcode;
+
+/**
+ * Created by Osipov on 04.04.2016.
+ */
+public class LauncherHash {
+    public static void main(String[] args) {
+        Person pers1 = new Person("Misha", 30, 2000);
+        Person pers2 = new Person("Misha", 36, 2000);
+        Person pers3 = new Person("Misha", 30, 2000);
+
+        System.out.println("1. " + pers1.getName() + " hash: " + pers1.hashCode());
+        System.out.println("2. " + pers2.getName() + " hash: " + pers2.hashCode());
+        System.out.println("3. " + pers3.getName() + " hash: " + pers3.hashCode());
+        System.err.println("1&3 hash 1: " + pers1.hashCode() + " 2: " + pers3.hashCode() + " and hash equal is " +
+                (pers1.hashCode() == pers3.hashCode()));
+    }
+
+}
---
+++ Lesson5/src/hashcode/Person.java
@@ -0,0 +1,51 @@
+package hashcode;
+
+/**
+ * Created by Osipov on 04.04.2016.
+ */
+public class Person {
+    private String name;
+    private int age;
+    private long salary;
+
+    @Override
+    public int hashCode() {
+        final int basis = 37;
+        int result = 71;
+
+        if(name == null) {
+            result = basis*result;
+        } else {
+            result = basis*result+name.hashCode();
+        }
+
+        Integer iAge = new Integer(age);
+        Long iSalary = new Long(salary);
+
+        result = basis*result+iAge.hashCode();
+        result = basis*result+iSalary.hashCode();
+
+        return result;
+    }
+
+    public Person() {
+    }
+
+    public Person(String name, int age, long salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public long getSalary() {
+        return salary;
+    }
+}
