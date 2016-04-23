3d838375fe643962d5c11160067489356f8fb17b
Mihail Osipov <osarch@hotbox.ru>
2016-04-23 16:51:30 +0300

    Clearing AI for Tank
    FirstWindow

--- Lesson6/src/jframe/FirstWindow.java
+++ Lesson6/src/jframe/FirstWindow.java
@@ -1,4 +1,12 @@
 package jframe;
 
+import javax.swing.*;
+
 public class FirstWindow {
+    public static void main(String[] args) {
+        JFrame firstWindow = new JFrame();
+
+        firstWindow.setBounds(300, 100, 800,600);
+        firstWindow.setVisible(true);
+    }
 }
