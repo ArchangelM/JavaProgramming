bed4ddb0e0d9c1fc1527cb31b759e07ca7e41575
Mihail Osipov <osarch@hotbox.ru>
2016-04-25 01:15:51 +0300

    load image

--- Lesson6/src/text/TextWindow.java
+++ Lesson6/src/text/TextWindow.java
@@ -3,26 +3,57 @@ package text;
 
 import com.sun.deploy.panel.JavaPanel;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.ImageObserver;
+import java.io.File;
+import java.io.IOException;
 
-public class TextWindow extends JavaPanel{
-     private JFrame window;
+public class TextWindow extends JPanel{
+    private final static String IMAGE_NAME = "sprites\\eagle64.png";
+    private Image image;
+
+    public JFrame window;
 
     public TextWindow() {
+        try {
+            image = ImageIO.read(new File(IMAGE_NAME));
+        } catch (IOException e) {
+            System.out.println("Can't find image - " + IMAGE_NAME);
+        }
+
+
         window = new JFrame("Text");
         window.setBounds(100, 100, 800, 600);
 
         window.getContentPane().add(this);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        window.pack();
+        //window.pack();
         window.setVisible(true);
     }
 
     @Override
-    protected void paintComponent(Graphics g) {
-        //super.paintComponent(g);
+    public void paint(Graphics g) {
+        super.paintComponent(g);
+
+        g.setColor(Color.BLUE);
+        g.fillRect(10, 10, 750, 550);
+
+        g.setColor(Color.GRAY);
+        g.draw3DRect(34, 67, 100, 100, true);
+
+        g.setColor(Color.ORANGE);
+        g.setFont(new Font(Font.MONOSPACED, Font.ITALIC, 12));
+        g.drawString("My first text field in the panel.", 200, 200);
+
+        g.drawImage(image, 300, 210, new ImageObserver() {
+            @Override
+            public boolean imageUpdate(Image img, int infoflags, int x, int y, int width, int height) {
+                return false;
+            }
+        });
 
     }
 
@@ -33,14 +64,14 @@ public class TextWindow extends JavaPanel{
     public static void main(String[] args) {
         TextWindow text = new TextWindow();
 
-        ///text.repaint();
-
+       text.repaint();
+/*
         JTextField myText = new JTextField("My first text field in the panel.");
 
         myText.setFont(new Font("Garamond", Font.BOLD, 16));
         text.addTextField(myText);
         text.repaint();
-
+/*
         JTextField myText1 = new JTextField("\nВнатуре!!!");
         myText1.setFont(new Font("Serif", Font.ITALIC, 12));
         myText1.setForeground(Color.CYAN);
@@ -49,6 +80,6 @@ public class TextWindow extends JavaPanel{
         text.addTextField(myText1);
 
         text.repaint();
-
+*/
     }
 }
