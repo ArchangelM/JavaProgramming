f929cd4444ff9e53c6b1093fa27a7ad516744ef8
Mihail Osipov <osarch@hotbox.ru>
2016-06-28 02:40:21 +0300

    Reflection UnitTest setPivate

--- Lesson8/test/reflection/ReflectionTest.java
+++ Lesson8/test/reflection/ReflectionTest.java
@@ -8,13 +8,18 @@ import org.junit.Test;
 import java.awt.*;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static reflection.Utils.setPrivates;
 
 
 @RunWith(JUnit4.class)
 public class ReflectionTest {
    //ApplicationManager appManager;
     List<Object> list;
+    Map<String, Object> map;
 
     @Before
     public void init() {
@@ -24,13 +29,17 @@ public class ReflectionTest {
         //list.add(new double[] {34d, 67d, 34,98});
         list.add(67.82d);
         list.add(new Color(23, 45, 169));
+
+        map = new HashMap<>();
+
+
     }
 
     @Test
     public void testInitClass() throws InvocationTargetException, NoSuchMethodException, InstantiationException,
             IllegalAccessException {
-        PrivateTest privateTestest = ApplicationManager.initClass(PrivateTest.class, Service.class, list);
-        Assert.assertNotNull("It`s null??? ", privateTestest);
+        PrivateTest privateTest = ApplicationManager.initClass(PrivateTest.class, Service.class, list);
+        Assert.assertNotNull("It`s null??? ", privateTest);
     }
 
     @Test
@@ -44,9 +53,9 @@ public class ReflectionTest {
     @Test
     public void testInitClassWrongAnnotation() throws InvocationTargetException, NoSuchMethodException, InstantiationException,
             IllegalAccessException {
-        PrivateTest privateTestest = ApplicationManager.initClass(PrivateTest.class, InitService.class, list);
+        PrivateTest privateTest = ApplicationManager.initClass(PrivateTest.class, InitService.class, list);
 
-        Assert.assertNull("WrongAnnotation ", privateTestest);
+        Assert.assertNull("WrongAnnotation ", privateTest);
     }
 
     @Test
@@ -54,8 +63,23 @@ public class ReflectionTest {
             IllegalAccessException {
 
         list.add(89l);
-        PrivateTest privateTestest = ApplicationManager.initClass(PrivateTest.class, Service.class, list);
+        PrivateTest privateTest = ApplicationManager.initClass(PrivateTest.class, Service.class, list);
 
-        Assert.assertNull("WrongParametrs ", privateTestest);
+        Assert.assertNull("WrongParametrs ", privateTest);
     }
+
+    @Test
+    public void testSetPrivatesWrongParametrs() throws InvocationTargetException, NoSuchMethodException, InstantiationException,
+            IllegalAccessException {
+
+        map.put("tInt", (Integer)12);
+        map.put("testDouble", 45.98);
+        map.put("color", new Color(23, 45, 169));
+        PrivateTest privateTest = setPrivates(PrivateTest.class, map);
+
+        Assert.assertEquals(45.98, privateTest.getTestDouble(), 0d);
+        Assert.assertEquals(12, privateTest.gettInt(), 0d);
+        Assert.assertEquals(new Color(23, 45, 169), privateTest.getColor());
+    }
+
 }
