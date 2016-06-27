542a82ae43968d8f79e52cbe84e346799f4e1729
Mihail Osipov <osarch@hotbox.ru>
2016-06-27 15:54:27 +0300

    Reflection5 Container Annotation

--- Lesson8/src/container/ServiceDescription.java
+++ Lesson8/src/container/ServiceDescription.java
@@ -1,5 +1,13 @@
 package container;
 
+/*
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+*/
+
 public class ServiceDescription implements Service {
     String type;
     @Override
@@ -17,4 +25,13 @@ public class ServiceDescription implements Service {
     public void setType(String type) {
         this.type = type;
     }
+/*
+    @Target(value = ElementType.TYPE)
+    @Retention(value = RetentionPolicy.RUNTIME)
+    //@Retention.RetentionPolicy.RUNTIME
+    @interface Service {
+        String name() default "runtimeCheck";
+    }
+*/
+
 }
