feb8d9494ae7768b979a32c8a03fc9a0e15d64bd
Mihail Osipov <osarch@hotbox.ru>
2016-07-05 02:03:45 +0300

    File print

--- Lesson9/src/stream/StreamsTest.java
+++ Lesson9/src/stream/StreamsTest.java
@@ -1,6 +1,13 @@
 package stream;
 
 import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 public class StreamsTest {
     public static void main(String[] args) {
@@ -9,6 +16,19 @@ public class StreamsTest {
         ByteArrayInputStream input = new ByteArrayInputStream(bytes);
 
         printStreamData(input);
+        System.out.println();
+
+        //InputStream inputStream = new FileInputStream(" ");
+        //Files files;
+        Path path = Paths.get("d:\\razrab\\test\\test.txt");
+        //InputStreamReader reader = new InputStreamReader(new FileInputStream(ваш_файл), "windows-1251");
+
+        try (InputStream inputS = Files.newInputStream(path)) {
+            printStreamData(inputS);
+        }catch (IOException e){
+            System.out.println(e);
+        }
+
 
     }
 
@@ -18,6 +38,19 @@ public class StreamsTest {
         while((cur = input.read()) != -1) {
             System.out.print((byte)cur);
             System.out.print(" ");
+        }
+    }
+
+    public static void printStreamData(InputStream input) {
+        int cur = 0;
+
+        try {
+            while ((cur = input.read()) != -1) {
+                System.out.print((char) cur);
+
+            }
+        } catch (IOException e) {
+            System.out.println(e);
         }
     }
 }
