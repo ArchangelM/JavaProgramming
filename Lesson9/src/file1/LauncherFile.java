b2189b72d1fa3da1e4f2880c236547c9c8d4d54f
Mihail Osipov <osarch@hotbox.ru>
2016-07-04 19:21:36 +0300

    stram print

---
+++ Lesson9/src/file1/LauncherFile.java
@@ -0,0 +1,23 @@
+package file1;
+
+import java.io.File;
+import java.io.IOException;
+
+public class LauncherFile {
+    public static void main(String[] args) throws IOException {
+        File file = new File("test.txt");
+
+
+        String path = "d:\\razrab";
+
+        PrintDir prnDir = new PrintDir();
+
+        //path = prnDir.creatingDir(path);
+       // prnDir.creatingFile(path);
+       // prnDir.printDir(path);
+
+
+
+    }
+
+}
---
+++ Lesson9/src/file1/PrintDir.java
@@ -0,0 +1,52 @@
+package file1;
+
+
+import java.io.File;
+import java.io.IOException;
+
+public class PrintDir {
+    public PrintDir() {
+
+    }
+
+    public void printDir(String path) {
+        path = path.replace("\\", File.separator);
+        File f = new File(path);
+        
+
+
+    }
+
+    public String creatingDir(String path) {
+        path = path.replace("\\", File.separator);
+        path = path + "\\test";
+
+        File f = new File(path);
+        System.out.println(f.getAbsolutePath());
+        if (f.mkdir()) {
+            System.out.println(f.getAbsolutePath());
+        } else {
+            System.out.println("Mistake");
+        }
+
+        return path;
+
+    }
+
+    public void creatingFile(String path) throws IOException {
+        path = path.replace("\\", File.separator);
+        path = path + "\\test.txt";
+
+        File f = new File(path);
+        System.out.println(f.getAbsolutePath());
+
+
+
+        if (f.createNewFile()) {
+            System.out.println(f.getAbsolutePath());
+        } else {
+            System.out.println("Mistake");
+        }
+
+    }
+}
